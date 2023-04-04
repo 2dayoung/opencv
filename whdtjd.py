89eaf52a163bf51778d76ce6602b143f0183ae9f
Lee-dayoung <daylee4564@naver.com>
2023-04-05 03:04:51 +0900

    트랙바 있고, 파란원 두개 검출되고 점수도 올라감

--- whdtjd.py
+++ whdtjd.py
@@ -45,35 +45,37 @@ while True:
     
     # 컨투어를 찾습니다.
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # 컨투어 중에서 파란색 원에 해당하는 원을 찾습니다.
+    blue_circles = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 1000:
+        # 컨투어를 감싸는 최소한의 원을 구합니다.
+            (x, y), radius = cv2.minEnclosingCircle(contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+                    # 파란색 원이라면 원 중심과 가운데 원의 거리를 구합니다.
+            if 110 <= hsv[int(y), int(x), 0] <= 130:
+                blue_circles.append((center, radius))
+                distance = np.sqrt((center_x - center[0]) ** 2 + (center_y - center[1]) ** 2)
+                
+                # 파란색 원과 가운데 원의 거리가 가운데 원의 반지름보다 작다면 점수를 1 증가시킵니다.
+                if distance < radius:
+                    score += 1
 
-    # 컨투어 중 가장 큰 영역을 찾습니다.
-    if contours:
-        max_contour = max(contours, key=cv2.contourArea)
-        
-        # 컨투어 내부에 파란색 원이 있는지 검사합니다.
-        M = cv2.moments(max_contour)
-        if M['m00'] != 0:
-            x = int(M['m10'] / M['m00'])
-            y = int(M['m01'] / M['m00'])
-            
-            # 컨투어를 둘러싸는 최소한의 사각형을 구합니다.
-            x,y,w,h = cv2.boundingRect(max_contour)
-            cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,0), 2)
-            
-            # 원을 그립니다.
-            cv2.circle(frame, (x, y), 20, (0, 0, 255), 2)
-            
-            # 원이 가운데 영역에 위치한 경우 점수를 1 증가시킵니다.
-            if (x-center_x)**2 + (y-center_y)**2 < radius**2:
-                score += 1
-        
-    # 가운데 원을 그립니다.
-    cv2.circle(frame, (center_x, center_y), radius, (255, 0, 0), 2)
+    # 파란색 원을 원본 이미지에 그립니다.
+    for circle in blue_circles:
+        center, radius = circle
+        cv2.circle(frame, center, radius, (255, 0, 0), 3)
 
-    # 점수를 출력합니다.
-    cv2.putText(frame, "Score: {}".format(score), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+    # 가운데 원을 원본 이미지에 그립니다.
+    cv2.circle(frame, (center_x, center_y), radius, (0, 255, 0), 3)
 
-    # 결과를 출력합니다.
+    # 현재 점수를 화면에 출력합니다.
+    cv2.putText(frame, 'Score: {}'.format(score), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+    # 결과를 보여줍니다.
     cv2.imshow('frame', frame)
 
     # ESC 키를 누르면 종료합니다.
