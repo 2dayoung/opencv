41a3aecb79030108275b1f19d9bb5164282c14d8
Lee-dayoung <daylee4564@naver.com>
2023-04-05 01:55:36 +0900

    파란원이 가운데에 가면 점수 올라감

--- whdtjd.py
+++ whdtjd.py
@@ -1,29 +1,47 @@
 import cv2
 import numpy as np
 
-#0이면 노트북 내장 웹캠 숫자를 올리면 추가된 웹캠을 이용할 수 있다.
 cap = cv2.VideoCapture(0)
-# 3은 가로 4는 세로 길이 
-cap.set(3, 720)
-cap.set(4, 1080)
+
+# 원의 중심 좌표와 반지름 범위를 설정합니다.
+blue_lower = np.array([100, 50, 50])
+blue_upper = np.array([130, 255, 255])
+center_x = 320
+center_y = 240
+radius = 100
+
+# 점수 변수를 초기화합니다.
+score = 0
 
 while True:
     ret, frame = cap.read()
-    frame1 = cv2.flip(frame,1)
-    gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    ret, thresh = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
-    contours, hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cnt = max(contours, key = lambda x: cv2.contourArea(x))
-    hull = cv2.convexHull(cnt)
-    drawing = np.zeros(frame.shape, np.uint8)
-    cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 2)
-    cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
-    cv2.imshow('output', drawing)
+
+    # 원 검출을 위해 BGR 이미지를 HSV로 변환합니다.
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # 파란색 원을 검출합니다.
+    mask = cv2.inRange(hsv, blue_lower, blue_upper)
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # 원이 검출되면 가장 큰 원을 찾습니다.
+    if len(contours) > 0:
+        cnt = max(contours, key=cv2.contourArea)
+        (x, y), r = cv2.minEnclosingCircle(cnt)
+        
+        # 원이 가운데 영역에 위치한 경우 점수를 1 증가시킵니다.
+        if (x-center_x)**2 + (y-center_y)**2 < radius**2:
+            score += 1
+
+        # 원을 그리고, 중심점을 표시합니다.
+        cv2.circle(frame, (int(x), int(y)), int(r), (0, 255, 0), 2)
+        cv2.circle(frame, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+    # 현재 점수를 화면에 출력합니다.
+    cv2.putText(frame, "Score: {}".format(score), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) == ord('q'):
         break
 
-
 cap.release()
-
 cv2.destroyAllWindows()
