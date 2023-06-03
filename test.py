5e1538d00cb8a7fac6e025f4682edd44c8905062
Lee-dayoung <daylee4564@naver.com>
2023-06-03 15:54:23 +0900

    마우스로 클릭해서 사다리꼴을 변환 함.

--- test.py
+++ test.py
@@ -1,198 +1,73 @@
 import cv2
-import mediapipe as mp
-import simpleaudio as sa
-import pygame.midi
-import time
-import mido
-import pygame
-#=========================
-#원래 있던 midi 파일 실행   play_music()
-#=========================
+import numpy as np
+
+# 마우스 이벤트 콜백 함수
+def mouse_callback(event, x, y, flags, param):
+    global roi_points, mouse_click_count
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        if mouse_click_count < 4:
+            roi_points.append((x, y))
+            mouse_click_count += 1
 
 
-#중복에 대해 진지하게 고민 
+# 건반 영역 변수 초기화
+roi_points = []
+mouse_click_count = 0
 
-def is_object_in_area(object_location, area):
-    x, y = object_location
-    start_x, start_y, width, height = area
-    return start_x <= x < start_x + width and start_y <= y < start_y + height
+# 건반 사진 불러오기
+image = cv2.imread("piano_image.jpg")
 
+# 건반 영역 선택을 위한 창 생성
+cv2.namedWindow("Piano")
+cv2.setMouseCallback("Piano", mouse_callback)
 
-# Mediapipe Hand Landmark 모델 초기화
-mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(static_image_mode=False, 
-                       max_num_hands=2, 
-                       min_detection_confidence=0.7, 
-                       min_tracking_confidence=0.7)
-
-
-
-# 웹캠 초기화
-cap = cv2.VideoCapture(0)
-cnt=0
-
-# 10개의 객체의 위치를 나타내는 리스트
-object_locations = [(2, 4), (5, 3), (1, 2), (4, 1), (3, 5), (5, 2), (2, 3), (1, 5), (3, 1), (4, 4)]
-
-# 5개의 영역을 나타내는 리스트, 각각의 영역은 (시작 x 좌표, 시작 y 좌표, 가로 길이, 세로 길이) 형태로 저장
-areas = [[53, 384, 53, 48], [106, 384, 53, 48], [159, 384, 53, 48],
- [212, 384, 53, 48], [265, 384, 53, 48], [318, 384, 53, 48],
- [371, 384, 53, 48], [424, 384, 53, 48], [477, 384, 53, 48],
- [530, 384, 53, 48]]
-
-
-
-prev_event = None
-arr_prev_event = None
-prev_event2 = None
-arr_prev_event2 = None
-prev_event1 = None
-arr_prev_event1 = None
-music_playing = False
-flag = False
-event = 0
-arr_event = []
-arr_event1 = []
-arr_event2 = []
-cnt = 0
 while True:
-    # 웹캠에서 프레임 읽기& 좌우반전 &크기
-    ret, frame = cap.read()
-    result= cv2.flip(frame,1) 
-    # frame = cv2.resize(frame,(920,720))
+    # 건반 영역 그리기
+    for i in range(len(roi_points)):
+        cv2.circle(image, roi_points[i], 5, (0, 0, 255), -1)
 
-    # 프레임을 RGB로 변환
-    image = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+    # 이미지 출력
+    cv2.imshow("Piano", image)
 
-    # Mediapipe Hand Landmark 모델을 사용하여 이미지 처리
-    results = hands.process(image)
-
-    if results.multi_hand_landmarks:
-        if len(results.multi_hand_landmarks) == 1:  # 손이 한손만 있을 때
-            handLms = results.multi_hand_landmarks[0]
-            
-        # 각 손가락 끝의 랜드마크 좌표 추출
-            fingertips = []
-            for finger_tip_id in [4, 8, 12, 16, 20]:
-                lm = handLms.landmark[finger_tip_id]
-                h, w, c = result.shape   #좌표가 0~1값임.화면상의 픽셀 좌표로 변환하기 위해 이미지의 크기필요 C는 채널
-                cx, cy = int(lm.x *w), int(lm.y*h)
-                fingertips.append((cx,cy))
-
-            #print('fingertips = ',fingertips)
-            # 손가락 끝에 원 그리기
-            outflag = False  
-            for location in  fingertips:              
-                cv2.circle(result, location, 5, (255, 0, 0), -1)  
-                #print('location = ',location)
-                         
-                for i in range(len(areas)):                   
-                    if is_object_in_area(location, areas[i]):
-                        # num = str(i)
-                        # cv2.putText(result, num, (100+i*50, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
-                        event = i
-                        arr_event.append(event)
-                        cnt =+ 1
-            #print ( arr_event)
-            if cnt != 0 :
-                #arr_event = [-1]
-                outflag = True
-            if not outflag :
-                arr_event = [-1]
-                
-           
-            # if arr_prev_event == arr_event:
-            #     flag = True
-            #     cnt = 0
-            #     continue
-            if arr_event != [-1] and not flag and arr_prev_event != arr_event:
-                print ( arr_event)
-                pass
-            #print ( arr_event)
-            arr_prev_event = arr_event 
-            arr_event = [] 
-            #print ( "prev",arr_prev_event)
-        else :      
-            handLms1 = results.multi_hand_landmarks[0]  # 왼손
-            handLms2 = results.multi_hand_landmarks[1]  # 오른손
-
-             # 각 손가락 끝의 랜드마크 좌표 추출 (왼손)
-            fingertips1 = []
-            for finger_tip_id in [4, 8, 12, 16, 20]:
-                lm = handLms1.landmark[finger_tip_id]
-                h, w, c = result.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                fingertips1.append((cx, cy))
-            
-            # 각 손가락 끝의 랜드마크 좌표 추출 (오른손)
-            fingertips2 = []
-            for finger_tip_id in [4, 8, 12, 16, 20]:
-                lm = handLms2.landmark[finger_tip_id]
-                h, w, c = result.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                fingertips2.append((cx, cy))
-            
-            outflag1 = False  
-            for location in  fingertips1:              
-                cv2.circle(result, location, 5, (255, 0, 0), -1)  
-
-                for i in range(len(areas)):                   
-                    if is_object_in_area(location, areas[i]):
-                        event = i
-                        arr_event1.append(event)
-                        cnt =+ 1
-            #print ( arr_event)
-            if cnt != 0 :
-                outflag1 = True
-            if not outflag1 :
-                arr_event1 = [-1]
-                
-            if arr_event1 != [-1] and not flag and arr_prev_event1 != arr_event1:
-                print ("Arr 1 ", arr_event1)
-                pass
-            #print ( arr_event)
-            arr_prev_event1 = arr_event1 
-            arr_event1 = [] 
-            #print ( "prev",arr_prev_event)
-
-            #오른손
-            outflag2 = False  
-            for location in  fingertips2:              
-                cv2.circle(result, location, 5, (255, 0, 0), -1)  
-
-                for i in range(len(areas)):                   
-                    if is_object_in_area(location, areas[i]):
-                        event = i
-                        arr_event2.append(event)
-                        cnt =+ 1
-            #print ( arr_event)
-            if cnt != 0 :
-                outflag2 = True
-            if not outflag2 :
-                arr_event2 = [-1]
-                
-            if arr_event2 != [-1] and not flag and arr_prev_event2 != arr_event2:
-                print ( "arr 2 ",arr_event2)
-                pass
-            #print ( arr_event)
-            arr_prev_event2 = arr_event2
-            arr_event2 = [] 
-            #print ( "prev",arr_prev_event)
-
-           
-    for area in areas:
-        cv2.rectangle(result, (area[0], area[1]),(area[0]+area[2],area[1]+area[3]), (0, 255, 0), 2)
-
-
-    # 결과 보여주기
-    cv2.imshow("Fingertip Detection division", result)
-
-    # 'q' 키를 누르면 루프 탈출
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # 'q' 키를 누르면 종료
+    if cv2.waitKey(1) & 0xFF == ord('q') or mouse_click_count >= 4:
         break
 
+# 건반 영역을 나타내는 4개의 점을 정렬
+roi_points = sorted(roi_points, key=lambda x: x[1])
 
-pygame.midi.quit()
-cap.release()
+# 사다리꼴을 형성하기 위한 좌표 계산
+top_left = roi_points[0]
+top_right = roi_points[1]
+bottom_right = roi_points[2]
+bottom_left = roi_points[3]
+
+# 사다리꼴 좌표
+src_points = np.float32([top_left, top_right, bottom_right, bottom_left])
+
+# 변환된 사다리꼴의 폭과 높이 계산
+width_top = np.linalg.norm(np.array(top_right) - np.array(top_left))
+width_bottom = np.linalg.norm(np.array(bottom_right) - np.array(bottom_left))
+height_left = np.linalg.norm(np.array(bottom_left) - np.array(top_left))
+height_right = np.linalg.norm(np.array(bottom_right) - np.array(top_right))
+
+# 변환된 사다리꼴의 폭과 높이 중 최댓값을 적용하여 결과 이미지 크기 계산
+max_width = max(int(width_top), int(width_bottom))
+max_height = max(int(height_left), int(height_right))
+
+# 결과 이미지 좌표
+dst_points = np.float32([[0, 0], [max_width - 1, 0], [max_width - 1, max_height - 1], [0, max_height - 1]])
+
+# Perspective Transform 행렬 계산
+transform_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+
+# Perspective Transform 적용
+result_image = cv2.warpPerspective(image, transform_matrix, (max_width, max_height))
+
+# 결과 출력
+cv2.imshow("Piano ROI", result_image)
+cv2.waitKey(0)
+
+# 종료
 cv2.destroyAllWindows()
-
