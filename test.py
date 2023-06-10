8b0ce1fdc91d74158df1e7f70ad1e7fb9bf566d6
Lee-dayoung <daylee4564@naver.com>
2023-06-11 01:55:17 +0900

    소리내는 함수 정리

--- test.py
+++ test.py
@@ -1,73 +1,41 @@
-import cv2
-import numpy as np
+import pyaudio
+import mido
+import struct
 
-# 마우스 이벤트 콜백 함수
-def mouse_callback(event, x, y, flags, param):
-    global roi_points, mouse_click_count
+# MIDI 파일 로드
+midi_file = "sample135.mid"
 
-    if event == cv2.EVENT_LBUTTONDOWN:
-        if mouse_click_count < 4:
-            roi_points.append((x, y))
-            mouse_click_count += 1
+# PyAudio 초기화
+p = pyaudio.PyAudio()
 
+# Stream 열기
+stream = p.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=44100,
+                output=True)
 
-# 건반 영역 변수 초기화
-roi_points = []
-mouse_click_count = 0
+# MIDI 파일 재생
+with mido.midifiles.MidiFile(midi_file) as midi:
+    for msg in midi.play():
+        if msg.type == 'note_on' or msg.type == 'note_off':
+            # 메시지를 MIDI 신호로 변환
+            note = msg.note
+            velocity = msg.velocity
+            duration = msg.time
 
-# 건반 사진 불러오기
-image = cv2.imread("piano_image.jpg")
+            # MIDI 신호를 오디오 신호로 변환
+            frequency = 440 * (2 ** ((note - 69) / 12))
+            samples_per_cycle = int(stream.get_sample_rate() / frequency)
+            samples = [velocity / 127.0 * 0.3 * (1 - i / samples_per_cycle % 1)
+                       for i in range(samples_per_cycle * duration)]
+            signal = b''.join([struct.pack('f', sample) for sample in samples])
 
-# 건반 영역 선택을 위한 창 생성
-cv2.namedWindow("Piano")
-cv2.setMouseCallback("Piano", mouse_callback)
+            # 오디오 재생
+            stream.write(signal)
 
-while True:
-    # 건반 영역 그리기
-    for i in range(len(roi_points)):
-        cv2.circle(image, roi_points[i], 5, (0, 0, 255), -1)
+# Stream 닫기
+stream.stop_stream()
+stream.close()
 
-    # 이미지 출력
-    cv2.imshow("Piano", image)
-
-    # 'q' 키를 누르면 종료
-    if cv2.waitKey(1) & 0xFF == ord('q') or mouse_click_count >= 4:
-        break
-
-# 건반 영역을 나타내는 4개의 점을 정렬
-roi_points = sorted(roi_points, key=lambda x: x[1])
-
-# 사다리꼴을 형성하기 위한 좌표 계산
-top_left = roi_points[0]
-top_right = roi_points[1]
-bottom_right = roi_points[2]
-bottom_left = roi_points[3]
-
-# 사다리꼴 좌표
-src_points = np.float32([top_left, top_right, bottom_right, bottom_left])
-
-# 변환된 사다리꼴의 폭과 높이 계산
-width_top = np.linalg.norm(np.array(top_right) - np.array(top_left))
-width_bottom = np.linalg.norm(np.array(bottom_right) - np.array(bottom_left))
-height_left = np.linalg.norm(np.array(bottom_left) - np.array(top_left))
-height_right = np.linalg.norm(np.array(bottom_right) - np.array(top_right))
-
-# 변환된 사다리꼴의 폭과 높이 중 최댓값을 적용하여 결과 이미지 크기 계산
-max_width = max(int(width_top), int(width_bottom))
-max_height = max(int(height_left), int(height_right))
-
-# 결과 이미지 좌표
-dst_points = np.float32([[0, 0], [max_width - 1, 0], [max_width - 1, max_height - 1], [0, max_height - 1]])
-
-# Perspective Transform 행렬 계산
-transform_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
-
-# Perspective Transform 적용
-result_image = cv2.warpPerspective(image, transform_matrix, (max_width, max_height))
-
-# 결과 출력
-cv2.imshow("Piano ROI", result_image)
-cv2.waitKey(0)
-
-# 종료
-cv2.destroyAllWindows()
+# PyAudio 종료
+p.terminate()
